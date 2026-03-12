232fdc28de3109ad0cb9df990e2e4c6570f55b22
Zhi.Huang <zh.impact@outlook.com>
2026-03-12 16:17:42 +0800

    update: hidden email subscribe

--- apps/frontend/src/pages/SubscribePage.tsx
+++ apps/frontend/src/pages/SubscribePage.tsx
@@ -13,7 +13,7 @@ export function SubscribePage() {
       </div>
 
       {/* Notice about email subscription being disabled */}
-      <div className="mb-6 bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg p-4">
+      <div className="mb-6 bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg p-4 hidden">
         <div className="flex items-start space-x-3">
           <svg className="w-6 h-6 text-yellow-600 dark:text-yellow-400 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
             <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0v3a1 1 0 002 0z" clipRule="evenodd" />
